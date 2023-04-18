d1b66b0096afeb9b7b239b733e05a337df4ef271
agrif22 <agoesrifqi@gmail.com>
2023-04-18 14:48:51 +0700

    Role Has Permissions

---
+++ api-gateway/routes/handler/roles/createPermission.js
@@ -0,0 +1,27 @@
+const apiAdapter = require('../../apiAdapter');
+const {
+    URL_SERVICE_USER
+} = process.env;
+
+const api = apiAdapter(URL_SERVICE_USER);
+
+module.exports = async (req, res) => {
+    try {
+        const role = await api.post('api/roles/rolehaspermissions', req.body);
+        return res.json(role.data);
+    } catch (error) {
+
+        if (error.code === "ECONNREFUSED") {
+            return res.status(500).json({
+                status: 'error',
+                message: 'Service Unavailable'
+            })
+        }
+
+        const {
+            status,
+            data
+        } = error.response;
+        return res.status(status).json(data);
+    }
+}
--- api-gateway/routes/handler/roles/index.js
+++ api-gateway/routes/handler/roles/index.js
@@ -3,11 +3,13 @@ const getAll = require('./getAll');
 const getRole = require('./getRole');
 const destroy = require('./destroy');
 const update = require('./update');
+const createPermission = require('./createPermission');
 
 module.exports = {
     create,
     getAll,
     getRole,
     destroy,
-    update
+    update,
+    createPermission,
 }
