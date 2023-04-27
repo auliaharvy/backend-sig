aaac45d1b506dbffef08f533a2af47437ba6dd80
agrif22 <agoesrifqi@gmail.com>
2023-04-27 11:43:27 +0700

    Lepas VerifyToken dan revisi Response get All user dari emaail menjadi email

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -5,9 +5,9 @@ const verifyToken = require('../middlewares/verifyToken');
 /* GET users listing. */
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
-router.put('/', verifyToken, userHandler.update);
-router.get('/:id', verifyToken, userHandler.getUser);
-router.get('/', verifyToken, userHandler.getAll);
-router.delete('/:id', verifyToken, userHandler.deleteUser);
-router.post('/logout', verifyToken, userHandler.logout);
+router.put('/', userHandler.update);
+router.get('/:id', userHandler.getUser);
+router.get('/', userHandler.getAll);
+router.delete('/:id', userHandler.deleteUser);
+router.post('/logout', userHandler.logout);
 module.exports = router;
